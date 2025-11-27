3fbf19e44959bf713eb7117750844a1a5002d49c
kimjunho-917 <yoisakimina@gmail.com>
2025-11-27 14:49:35 +0900

    함수 작성

---
+++ 단위 변환 프로그램.py
@@ -0,0 +1,11 @@
+def cm_to_inch(cm):
+    return cm / 2.54
+
+def inch_to_cm(inch):
+    return inch * 2.54
+
+def kg_to_lb(kg):
+    return kg * 2.20462262
+
+def lb_to_kg(lb):
+    return lb / 2.20462262
