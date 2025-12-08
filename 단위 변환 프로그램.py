67b3d4c34dd69cf7fc42aeb0b878495777c881ca
kimjunh-917 <yoisakimina@gmail.com>
2025-12-08 09:39:53 +0900

    화씨 섭시

--- 단위 변환 프로그램.py
+++ 단위 변환 프로그램.py
@@ -1,11 +1,5 @@
-def cm_to_inch(cm):
-    return cm / 2.54
+def c_to_f(c):
+    return (c * 9/5) + 32
 
-def inch_to_cm(inch):
-    return inch * 2.54
-
-def kg_to_lb(kg):
-    return kg * 2.20462262
-
-def lb_to_kg(lb):
-    return lb / 2.20462262
+def f_to_c(f):
+    return (f - 32) * 5/9
